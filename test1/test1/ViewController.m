be24421f45d98822ca0b249ee4d410b6b4ffdb33
FuErKang <wustgs1990@gmail.com>
2015-12-01 10:21:31 +0800

    first

---
+++ test1/test1/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  test1
+//
+//  Created by tarena01_05 on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"shit");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
